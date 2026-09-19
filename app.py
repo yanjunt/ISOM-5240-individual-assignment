710369da72b62926af82492f88b57232bbb39870
yanjunt <ytuan@connect.ust.hk>
2026-09-19 19:34:28 +0800

    Enhance app with title, welcome message, and image uploader
    Added a title and welcome message to the app, and included an image uploader.

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from PIL import Image
 from transformers import BlipProcessor, BlipForConditionalGeneration, pipeline
 from gtts import gTTS
 
+st.title("story telling demo")
+st.write("welcome to a story telling app for children")
+
+uploaded_image=st.feile_uploader("Upload an image",type=["jpg","jpeg","png"])
 @st.cache_resource
 def load_captioner():
     proc = BlipProcessor.from_pretrained("Salesforce/blip-image-captioning-base")
